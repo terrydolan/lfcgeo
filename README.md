7bd598ce05f6bdd1063de84eeb1386802a3c60b9
Terry <terrydolan@users.noreply.github.com>
2020-10-06 21:43:46 +0100

    added animation

--- README.md
+++ README.md
@@ -3,18 +3,18 @@ lfcgeo is a web app that maps each LFC squad player's shortest journey from thei
 
 The app illustrates the changing nature of top flight football in England. You can see that most of the players from Liverpool's first squad in 1892-1893 came from Scotland. Indeed that team was known as the '*team of macs*'. Liverpool's 2020-2021 squad is truly global, with players from 19 different countries who have travelled over 42,000 miles to reach Anfield.
 
-The app has knowledge of every player that has been included in an LFC squad in every season; that is 100+ seasons and 800+ Liverpool players.
+The app has knowledge of every player that has been included in an LFC squad; that is 100+ seasons and 800+ players.
 
 ## Try the app
 The lfcgeo app is deployed at [lfcgeo.herokuapp.com](https://lfcgeo.herokuapp.com). It is also available from [lfcgeo.lfcsorted.com](http://lfcgeo.lfcsorted.com).
 
-## App user experience
-The app maps each LFC season squad player's shortest journey from their birthplace to Anfield using a 'great-circle' line of shortest distance. You can select the season to map using a slider. Any LFC season can be selected, from 1892-1893 to the current season. The default selection is 1962-1963, the season Shankly's Liverpool team returned to the top flight.
+## App user interface
+The app maps each LFC season squad player's shortest journey from their birthplace to Anfield using a 'great-circle' line of shortest distance. You can select the season to map using a slider. Any LFC season can be selected, from 1892-1893 to the current season. The default selection is 1900-01, Liverpool's first title winning season.
 
 The generated line from a player's birthplace to Anfield is interactive, if you 'hover' over the line then that player's journey details are displayed. You can also zoom in and out of the map. For example you can zoom in to Anfield to see the player's from Liverpool in a given season; or you can zoom out to the world view. The selected season's dataframe is also available to view below the map.
 
 ## Primary data source
-The LFC squad and player data is sourced from the excellent[www.lfchistory.net](http://www.lfchistory.net). 
+The LFC squad and player data is sourced from the excellent [www.lfchistory.net](http://www.lfchistory.net). 
 
 ## Data preparation and app prototyping
 The data was cleansed, enriched and mapped using jupyter, python, pandas, geopy and pydeck with mapbox. The geo-location of each player's bithplace is determined using geopy's geocode with openstreetmap's Nominatim API.
--- lfcgeo_app.py
+++ lfcgeo_app.py
@@ -33,16 +33,18 @@ logging.config.dictConfig(lfcgeo_log_config.dictLogConfig)
 logger = logging.getLogger('lfcgeo')
 
 # define defaults
-MODE_RUN = 'Run the app'
-MODE_ABOUT = 'About the app'
+MODE_RUN = 'Run the lfcgeo app'
+MODE_ABOUT = 'About the lfcgeo app'
+MODE_ANI = 'Run the PTWR animation'
+
 ABOUT_MD_FILE = 'README.md'
 SHOW_DATAFRAME = True
 SQUAD_PLAYERS_GEO_CSV_FILE = 'data/dflfc_squad_players_geo_Sep2020.csv'
 ANFIELD_LATITUTE = 53.4308358
 ANFIELD_LONGITUDE = -2.9609095414165294
-DEFAULT_SEASON = '1962-1963' # Shankly's first season back in the top flight, and somewhere in the middle!
-ZOOM_MAX = 11
-ZOOM_MIN = 0.5
+DEFAULT_SEASON = '1900-1901' # Liverpool's first title winning season
+ZOOM_ANFIELD = 11
+ZOOM_WORLD = 0.6
 ZOOM_DEFAULT = 4
 GREEN = [0, 255, 0] # RGB
 RED = [255, 0, 0] # RGB
@@ -50,7 +52,13 @@ WHITE = [255, 255, 255] # RGB
 LINE_WIDTH=7
 PICKING_RADIUS = 5*LINE_WIDTH
 MAP_STYLE = 'mapbox://styles/mapbox/dark-v10' # ref: https://docs.mapbox.com/api/maps
+ANI_MP4 = "LFC PTWR 1892-1893 to 2020-2021.mp4"
 
+# set initial values
+view_zoom_level = ZOOM_DEFAULT
+view_latitude = ANFIELD_LATITUTE
+view_longitude = ANFIELD_LONGITUDE
+        
 # define helper functions
 @st.cache
 def read_lfcgeo_df():
@@ -67,21 +75,20 @@ def read_about_md(filename):
 
 # define page config
 st.beta_set_page_config(
-    page_title="lfcgeo",
+    page_title="lfcgeo proto",
     page_icon="redglider.ico",
     layout="wide",
-    initial_sidebar_state="collapsed")
+    initial_sidebar_state="expanded") #collapsed
 
 # define sidebar to select mode (default mode is to run the app)
-select_mode = st.sidebar.radio('Select mode:', (MODE_RUN, MODE_ABOUT), index=0)
+select_mode = st.sidebar.radio('Select mode:', (MODE_RUN, MODE_ABOUT, MODE_ANI), index=0)
 
 st.sidebar.markdown(f"*lfcgeo, version {__version__}*")
-
 
 if select_mode == MODE_RUN:
    
     # introduce the web app
-    st.title("lfcgeo")
+    st.title("lfcgeo proto")
     st.header("Map each LFC squad player's shortest journey from their birthplace to Anfield")
 
     # read all squad players' data, with geo and tooltip
@@ -93,18 +100,6 @@ if select_mode == MODE_RUN:
                                        options=seasons,
                                        value=DEFAULT_SEASON)
     st.write(f"{selected_season} - the map is interactive, hover over a line to see player details; zoom in and out")
-
-##    # show quick zoom radio buttons
-##    # not used as issues with streamlit positioning of Anfield when moving to max zoom"
-##    # zoom fixed at default for now
-##    zoom_choice = st.sidebar.radio('Select a pre-set zoom:',
-##                                   ('Zoom in to Anfield view', 'Zoom out to world view', 'Zoom default view'), index=2)
-##    if zoom_choice == 'Zoom in to Anfield view':
-##        zoom_level = ZOOM_MAX
-##    elif zoom_choice == 'Zoom out to world view':
-##        zoom_level = ZOOM_MIN
-##    else:
-##        zoom_level = ZOOM_DEFAULT
 
     # filter selected season in dataframe and prepare dataframe for mapping
     FILTER = (dflfc_squad_player_geo.Season == selected_season)
@@ -128,13 +123,14 @@ if select_mode == MODE_RUN:
         highlightColor=WHITE
     )
 
-    # Set the viewport location
-    view_state = pdk.ViewState(latitude=ANFIELD_LATITUTE, longitude=ANFIELD_LONGITUDE, zoom=ZOOM_DEFAULT, bearing=0, pitch=0)
+    # set the viewport location
+    view_state = pdk.ViewState(latitude=view_latitude, longitude=view_longitude, zoom=view_zoom_level, bearing=0, pitch=0)
 
-    # Render
+    # render
     logger.info(f"Render the lfcgeo map for season: {selected_season}")
     r = pdk.Deck(layers=[layer], initial_view_state=view_state, map_style=MAP_STYLE,
-                 tooltip={"text": "{Source_name} \nto {Target_name} ({Anfield_dist_mi_rnd} miles)"})
+                 tooltip={"text": "{Player}\nfrom {Birthplace_upd}\nto Anfield ({Anfield_dist_mi_rnd} miles)"})
+    r.picking_radius=2*PICKING_RADIUS
     st.pydeck_chart(r)
     st.markdown("_Data source: [lfchistory.net](https://www.lfchistory.net/)_")
 
@@ -145,9 +141,14 @@ if select_mode == MODE_RUN:
             logger.info(f"Show the dataframe for season: {selected_season}")
             st.write(df_plot[['Season', 'Player', 'Birthplace_upd', 'Source_coords_xfm']]\
                      .rename(columns={'Birthplace_upd': 'Birthplace', 'Source_coords_xfm': 'Geo-location'}))
-    
-else:
+
+elif select_mode == MODE_ABOUT:
     logger.info('Show info about lfcgeo')
     st.markdown(read_about_md(ABOUT_MD_FILE))
+
+elif select_mode == MODE_ANI:
+    logger.info('Show PTWR animation')
+    st.video(ANI_MP4)
+    st.markdown("_Data source: [lfchistory.net](https://www.lfchistory.net/)_")
 
     
